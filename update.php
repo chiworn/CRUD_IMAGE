b91fb7357ecaa638940a6c40d442e96eb923f607
Chi Worn <chiworn@example.com>
2026-08-31 13:33:38 +0700

    updat again

--- update.php
+++ update.php
@@ -1,4 +1,5 @@
 <?php 
+include "./db.php";
 echo $_SERVER['REQUEST_METHOD'];
 
 
@@ -11,9 +12,32 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     $dis = $_POST['dis'];
 
     $oldimage = $_POST['oldimage'];
-    echo empty($Num);
+    echo $oldimage;
     
 
+    if( !empty($_FILES['image']['name']) ){
 
+    if(file_exists("./uploads/$oldimage")){
+        unlink("./uploads/$oldimage");
+    }
+
+    $newimage = $_FILES['image']['name'];
+    $path     = $_FILES['image']['tmp_name'];
+
+    $newname = time().$newimage;
+    move_uploaded_file($path,"./uploads/$newname");
+
+    $con->query("UPDATE `tbl_products` SET `namepro`='$name',`price`='$price',`qty`='$qty',`dis`='$dis',`image`='$newname' WHERE `id` =  $id");
+
+ 
+    }
+   
+    $con->query("UPDATE `tbl_products` SET `namepro`='$name',`price`='$price',`qty`='$qty',`dis`='$dis',`image`='$oldimage' WHERE `id` =  $id");
+
+    header("location: index.php");
+    exit;
+    
+     
+   
 }
 ?>
